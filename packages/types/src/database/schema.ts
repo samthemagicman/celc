98ffab7340f698c8018e1d1beebf846f84bfad23
Sam Salfi <samsalfi@yahoo.ca>
2024-10-27 12:16:36 -0300

    Added description to DB schema

--- packages/types/src/database/schema.ts
+++ packages/types/src/database/schema.ts
@@ -3,6 +3,7 @@ import { int, real, sqliteTable, text } from "drizzle-orm/sqlite-core";
 export const event = sqliteTable("event", {
   id: int().primaryKey({ autoIncrement: true }),
   title: text().notNull(),
+  description: text(),
   location: text().notNull(),
   startHour: real().notNull(),
   endHour: real().notNull(),
