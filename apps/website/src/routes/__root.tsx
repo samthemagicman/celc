9d65fc85ca4beddd8d2b9e32e260253cea47daf7
Sam Salfi <samsalfi@yahoo.ca>
2024-11-17 10:40:11 -0400

    Added linting to build action

--- apps/website/src/routes/__root.tsx
+++ apps/website/src/routes/__root.tsx
@@ -4,35 +4,37 @@ import { useAuth } from "~/components/auth";
 import { Button } from "~/components/ui/button";
 
 export const Route = createRootRoute({
-  component: () => {
-    const auth = useAuth();
-    const isLoggedIn = useAuth((s) => s.loggedIn);
-    function login() {
-      auth.startDiscordLogin();
-    }
-    function logout() {
-      auth.logout();
-    }
-    return (
-      <>
-        <div className="p-2 flex gap-2">
-          <Link to="/" className="[&.active]:font-bold">
-            Home
-          </Link>{" "}
-          <Link to="/about" className="[&.active]:font-bold">
-            About
-          </Link>
-          <div className="w-full" />
-          {!isLoggedIn ? (
-            <Button onClick={login}>Login</Button>
-          ) : (
-            <Button onClick={logout}>Logout</Button>
-          )}
-        </div>
-        <hr />
-        <Outlet />
-        <TanStackRouterDevtools />
-      </>
-    );
-  },
+  component: () => Root,
 });
+
+export const Root = () => {
+  const auth = useAuth();
+  const isLoggedIn = useAuth((s) => s.loggedIn);
+  function login() {
+    auth.startDiscordLogin();
+  }
+  function logout() {
+    auth.logout();
+  }
+  return (
+    <>
+      <div className="p-2 flex gap-2">
+        <Link to="/" className="[&.active]:font-bold">
+          Home
+        </Link>{" "}
+        <Link to="/about" className="[&.active]:font-bold">
+          About
+        </Link>
+        <div className="w-full" />
+        {!isLoggedIn ? (
+          <Button onClick={login}>Login</Button>
+        ) : (
+          <Button onClick={logout}>Logout</Button>
+        )}
+      </div>
+      <hr />
+      <Outlet />
+      <TanStackRouterDevtools />
+    </>
+  );
+};
