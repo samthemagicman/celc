64ccf2f25790017e55d796f15e60958a3a546a64
Sam Salfi <samsalfi@yahoo.ca>
2024-12-15 18:49:27 -0400

    updated navbar

---
+++ apps/website/src/components/navbar/index.tsx
@@ -0,0 +1,169 @@
+import { Cross1Icon, HamburgerMenuIcon } from "@radix-ui/react-icons";
+import { Link, useMatchRoute } from "@tanstack/react-router";
+import React from "react";
+import { createPortal } from "react-dom";
+import { useAuth } from "~/components/auth";
+import { useBreakpoint } from "~/hooks/tailwind";
+import { cn } from "~/lib/utils";
+import { Button } from "../ui/button";
+
+const navbarLinks = [
+  { to: "/", label: "Events" },
+  { to: "/admin", label: "Admin", role: "admin" },
+  { to: "/my-calendar", label: "My Calendar", role: "user" },
+];
+
+const HamburgerButton: React.FC<{ onClick: () => void; active?: boolean }> = ({
+  onClick,
+  active,
+}) => {
+  return (
+    <button onClick={onClick} className="z-50">
+      {active ? (
+        <Cross1Icon height={32} width={32} className="text-[#c4326d]" />
+      ) : (
+        <HamburgerMenuIcon height={32} width={32} />
+      )}
+    </button>
+  );
+};
+
+const NavbarLink: React.FC<{
+  to: string;
+  children?: React.ReactNode;
+  className?: string;
+  onClick?: () => void;
+}> = ({ to, children, className, onClick }) => {
+  const ref = React.useRef<HTMLAnchorElement>(null);
+  let isActive = false;
+  const matchRoute = useMatchRoute();
+  const params = matchRoute({ to: to });
+
+  if (params) {
+    isActive = true;
+  }
+
+  return (
+    <Link
+      onClick={onClick}
+      ref={ref}
+      to={to}
+      className={cn(
+        "py-2 flex-none leading-tight",
+        {
+          "underline underline-offset-8": isActive,
+        },
+        className,
+      )}
+    >
+      {children}
+    </Link>
+  );
+};
+
+export const MobileNavMenu: React.FC<{ onButtonClicked: () => void }> = ({
+  onButtonClicked,
+}) => {
+  const auth = useAuth();
+  const jwt = auth.getJwtPayload();
+  const isLoggedIn = useAuth((s) => s.isLoggedIn)();
+  function login() {
+    auth.startDiscordLogin();
+  }
+  function logout() {
+    auth.logout();
+  }
+  return createPortal(
+    <div className="fixed inset-0 bg-[#ffdd65] text-[#c4326d] z-40 flex items-center justify-center flex-col gap-8 text-4xl font-semibold">
+      {navbarLinks.map((link) => {
+        if (link.role && link.role !== jwt?.role) {
+          return null;
+        }
+        return (
+          <NavbarLink key={link.to} to={link.to} onClick={onButtonClicked}>
+            {link.label}
+          </NavbarLink>
+        );
+      })}
+      {!isLoggedIn ? (
+        <Button
+          onClick={login}
+          className="h-auto w-auto text-4xl bg-[#c4326d] hover:bg-[#c4326d]/90"
+        >
+          Login
+        </Button>
+      ) : (
+        <Button
+          onClick={logout}
+          className="h-auto w-auto text-4xl bg-[#c4326d] hover:bg-[#c4326d]/90"
+        >
+          Logout
+        </Button>
+      )}
+    </div>,
+    document.body,
+  );
+};
+
+export const Navbar: React.FC = () => {
+  const auth = useAuth();
+  const jwt = auth.getJwtPayload();
+  const isMobile = !useBreakpoint("sm");
+  const [menuOpen, setMenuOpen] = React.useState(false);
+  const isLoggedIn = useAuth((s) => s.isLoggedIn)();
+  function login() {
+    auth.startDiscordLogin();
+  }
+  function logout() {
+    auth.logout();
+  }
+
+  return (
+    <div className="py-2 px-4 bg-[#212121] text-white">
+      <div className="max-w-screen-xl mx-auto flex items-center gap-4">
+        <img src="/celc-logo.png" alt="logo" className="h-12 z-50" />
+        {!isMobile && (
+          <div className="flex text-white items-center gap-8 pl-4 flex-1 justify-center md:justify-start">
+            {navbarLinks.map((link) => {
+              if (link.role && link.role !== jwt?.role) {
+                return null;
+              }
+              return (
+                <NavbarLink key={link.to} to={link.to}>
+                  {link.label}
+                </NavbarLink>
+              );
+            })}
+          </div>
+        )}
+        {isMobile ? (
+          <>
+            <div className="flex-1" />
+            <HamburgerButton
+              onClick={() => {
+                setMenuOpen(!menuOpen);
+              }}
+              active={menuOpen}
+            />
+          </>
+        ) : (
+          <>
+            {!isLoggedIn ? (
+              <Button onClick={login}>Login</Button>
+            ) : (
+              <Button onClick={logout}>Logout</Button>
+            )}
+          </>
+        )}
+
+        {isMobile && menuOpen && (
+          <MobileNavMenu
+            onButtonClicked={() => {
+              setMenuOpen(false);
+            }}
+          />
+        )}
+      </div>
+    </div>
+  );
+};
--- apps/website/src/routes/__root.tsx
+++ apps/website/src/routes/__root.tsx
@@ -1,7 +1,6 @@
-import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
+import { createRootRoute, Outlet } from "@tanstack/react-router";
 import React from "react";
-import { useAuth } from "~/components/auth";
-import { Button } from "~/components/ui/button";
+import { Navbar } from "~/components/navbar";
 
 const TanStackRouterDevtools =
   process.env.NODE_ENV === "production"
@@ -16,39 +15,9 @@ const TanStackRouterDevtools =
       );
 
 const MainRoute = () => {
-  const auth = useAuth();
-  const isLoggedIn = useAuth((s) => s.isLoggedIn)();
-  const jwt = auth.getJwtPayload();
-  function login() {
-    auth.startDiscordLogin();
-  }
-  function logout() {
-    auth.logout();
-  }
   return (
     <>
-      <div className="py-2 px-4 flex gap-4 items-center">
-        <Link to="/" className="[&.active]:font-bold">
-          Home
-        </Link>{" "}
-        {jwt?.role === "admin" && (
-          <Link to="/admin" className="[&.active]:font-bold">
-            Admin
-          </Link>
-        )}
-        {/* {Conditional rendering cool stuff} */}
-        {isLoggedIn && (
-          <Link to="/my-calendar" className="[&.active]:font-bold">
-            My Calendar
-          </Link>
-        )}
-        <div className="w-full" />
-        {!isLoggedIn ? (
-          <Button onClick={login}>Login</Button>
-        ) : (
-          <Button onClick={logout}>Logout</Button>
-        )}
-      </div>
+      <Navbar />
       <hr />
       <Outlet />
       <TanStackRouterDevtools />
